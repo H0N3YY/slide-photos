1c23820c0dd8ec4509cd7b66c6e2083f49d3524b
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-03-20 12:13:55 +0100

    install-and-make-base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { productData, responsive } from "./components/Data";
+import Towns from "./components/Towns";
 
-function App() {
+export default function App() {
+  const product = productData.map((item) => (
+    <Towns name={item.name} url={item.imageAdress} />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React multi carousel</h1>
+      <Carousel showDots={true} responsive={responsive}>
+        {product}
+      </Carousel>
     </div>
   );
 }
-
-export default App;
