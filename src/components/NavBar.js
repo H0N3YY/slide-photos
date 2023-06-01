9c0fc949f81854124b8ea6e64837b16765e0189d
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-01 17:16:34 +0200

    change navbar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   AppBar,
   Toolbar,
@@ -5,27 +6,46 @@ import {
   Typography,
   Stack,
   Button,
-} from "@mui/material";
-import MapIcon from "@mui/icons-material/Map";
+} from '@mui/material';
 import { brown } from '@mui/material/colors';
+import MapIcon from '@mui/icons-material/Map';
 
 const Header = () => {
+  const appBarStyle = {
+    backgroundColor: brown[400],
+  };
+
+  const logoStyle = {
+    marginRight: '1rem',
+  };
+
+  const titleStyle = {
+    flexGrow: 1,
+    fontWeight: 'bold',
+  };
+
   return (
-    <AppBar position="static" sx={{backgroundColor: brown[400]}}>
+    <AppBar position="static" sx={appBarStyle}>
       <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="logo"
+          style={logoStyle}
+        >
           <MapIcon />
         </IconButton>
-        <Typography variant="h6" component="div">
+        <Typography variant="h6" component="div" style={titleStyle}>
           TRAVELS POL
         </Typography>
-
-        <Stack direction="row" spacing={2}> 
-          <Button color="inherit"> ZDJĘCIA </Button>
-          <Button color="inherit"> KONTAKT </Button>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">ZDJĘCIA</Button>
+          <Button color="inherit">KONTAKT</Button>
         </Stack>
       </Toolbar>
     </AppBar>
   );
 };
+
 export default Header;
