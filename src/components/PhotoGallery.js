34d929ee282e07f68616f6153e86365e7bf49827
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-01 17:14:56 +0200

    change gallery style

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,16 +1,31 @@
-import { Box,Typography } from "@mui/material";
-import classes from "./PhotoGalleryStyle.css";
-import StandardImageList from "./ImageList";
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 import { grey, brown } from '@mui/material/colors';
+import StandardImageList from './ImageList';
 
 const PhotoGallery = () => {
+  const galleryContainerStyle = {
+    backgroundColor: grey[50],
+    padding: '2rem',
+  };
+
+  const headingStyle = {
+    backgroundColor: brown[400],
+    color: '#fff',
+    padding: '1rem',
+    marginBottom: '2rem',
+    fontSize: '2rem',
+    textAlign: 'center',
+  };
+
   return (
-    <Box className={classes.gallery_container} sx={{backgroundColor: grey[50]}}>
-      <Typography variant="h2" component="div" padding={5} sx={{backgroundColor: brown[400]}}>
-        Nasza Galleria
+    <Box style={galleryContainerStyle}>
+      <Typography variant="h2" component="div" style={headingStyle}>
+        Nasza Galeria
       </Typography>
       <StandardImageList />
     </Box>
   );
 };
+
 export default PhotoGallery;
