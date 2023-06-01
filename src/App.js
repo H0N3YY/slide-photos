2f96a612422ff9352ae128b75f42f70f6dccaedb
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-01 17:22:40 +0200

    change elements carousel style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import { townsPhotos, responsive } from "./components/TownPictures";
-import { Typography } from "@mui/material";
-
-import "./App.css";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import Towns from "./components/Towns";
-import Header from "./components/NavBar";
-import PhotoGallery from "./components/PhotoGallery";
+import React from 'react';
+import { Typography } from '@mui/material';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Header from './components/NavBar';
+import PhotoGallery from './components/PhotoGallery';
+import { townsPhotos, responsive } from './components/TownPictures';
 import { brown } from '@mui/material/colors';
+import Towns from "./components/Towns";
+
+import './App.css';
 
 export default function App() {
   const product = townsPhotos.map((item) => (
     <Towns name={item.name} url={item.imageAdress} />
   ));
 
+  const carouselTitleStyle = {
+    color: brown[900],
+    padding: '1rem',
+    fontWeight: 'bold',
+    fontSize: '2rem',
+    textAlign: 'center',
+  };
+
   return (
-    <div className="App" >
+    <div className="App">
       <Header />
       <div className="carousel">
-        <Typography variant="h2" component="div" padding={3} sx={{color: brown[900]}}>
+        <Typography variant="h2" component="div" sx={carouselTitleStyle}>
           Najlepsze Oferty na Weekend!
         </Typography>
         <Carousel showDots={false} responsive={responsive}>
--- src/components/Towns.js
+++ src/components/Towns.js
@@ -1,16 +1,44 @@
-import React from "react";
-import { Button, Box, Typography } from "@mui/material";
+import React from 'react';
+import { Button, Box, Typography } from '@mui/material';
 import { brown } from '@mui/material/colors';
 
 export default function Towns(props) {
+  const cardStyle = {
+    backgroundColor: '#F5F5F5',
+    padding: '1rem',
+    borderRadius: '10px',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+  };
+
+  const townImageStyle = {
+    width: '80',
+    height: '80',
+    marginBottom: '1rem',
+    borderRadius: '5px',
+  };
+
+  const townNameStyle = {
+    color: brown[900],
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    marginBottom: '0.5rem',
+  };
+
+  const buttonStyle = {
+    color: brown[500],
+    fontWeight: 'bold',
+  };
+
   return (
-    <div className="card">
-      <Box sx={{color: brown[900]}}>
-      <img className="town_image" src={props.url} alt="town" />
-      <Typography variant="h4" paddingTop={2}>{props.name}</Typography>
-      <p>
-        <Button sx={{color: brown[500]}}> Sprawdź Oferte </Button>
-      </p>
+    <div className="card" style={cardStyle}>
+      <Box sx={{ color: brown[900] }}>
+        <img className="town_image" src={props.url} alt="town" style={townImageStyle} />
+        <Typography variant="h4" component="div" sx={townNameStyle}>
+          {props.name}
+        </Typography>
+        <p>
+          <Button sx={buttonStyle}>Sprawdź Ofertę</Button>
+        </p>
       </Box>
     </div>
   );
