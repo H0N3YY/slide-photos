ecaadbf9af2fa5c5a3d057f6951c5433fad1c063
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-23 12:43:48 +0200

    base for change style

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, TextField, Button, Grid } from '@mui/material';
+import { brown } from '@mui/material/colors';
 
 const ContactForm = () => {
     const handleSubmit = (e) => {
@@ -7,9 +8,38 @@ const ContactForm = () => {
         // Logika obsługi wysyłania formularza
     };
 
+    const formStyle = {
+        backgroundColor: '#F5F5F5',
+        padding: '1rem',
+        borderRadius: '10px',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+    };
+
+    const textFieldStyle = {
+        marginBottom: '1rem',
+    };
+
+    const buttonStyle = {
+        '&:hover': {
+            backgroundColor: brown[800],
+            color: 'white',
+        },
+        color: brown[500],
+        fontWeight: 'bold',
+        borderRadius: '4em',
+        width: '50%',
+    };
+
+    const headingStyle = {
+        color: brown[900],
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        marginBottom: '0.5rem',
+    };
+
     return (
-        <div>
-            <Typography variant="h4" gutterBottom>
+        <div style={formStyle}>
+            <Typography variant="h4" gutterBottom style={headingStyle}>
                 Formularz kontaktowy
             </Typography>
             <form onSubmit={handleSubmit}>
@@ -21,6 +51,7 @@ const ContactForm = () => {
                             label="Imię"
                             name="firstName"
                             variant="outlined"
+                            style={textFieldStyle}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -30,6 +61,7 @@ const ContactForm = () => {
                             label="Nazwisko"
                             name="lastName"
                             variant="outlined"
+                            style={textFieldStyle}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -40,6 +72,7 @@ const ContactForm = () => {
                             name="email"
                             type="email"
                             variant="outlined"
+                            style={textFieldStyle}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -51,10 +84,11 @@ const ContactForm = () => {
                             label="Wiadomość"
                             name="message"
                             variant="outlined"
+                            style={textFieldStyle}
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <Button type="submit" variant="contained" color="primary">
+                        <Button type="submit" variant="contained" color="primary" style={buttonStyle}>
                             Wyślij wiadomość
                         </Button>
                     </Grid>
